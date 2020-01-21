36e25dbeefec4bcfff2eab1e8c78bb0208369773
Minhazul Hayat Khan <minhaz1217@users.noreply.github.com>
2020-01-22 01:25:23 +0600

    now context menu item is showing only on 9gag website

--- 9gag_media_download_helper.js
+++ 9gag_media_download_helper.js
@@ -1,54 +1,56 @@
+
 browser.contextMenus.create({
     id: "download-9gag-media",
     title: "Download 9gag",
     contexts: ["image", "video"],
+    documentUrlPatterns  : ["*://*.9gag.com/*"]
 });
 
 browser.contextMenus.onClicked.addListener((info, tab) => {
 
+    // console.log("Info\n");
+    // console.log(info);
+    // console.log("TAB\n");
+    // console.log(tab);
+    // console.log(info.pageUrl);
+    // console.log(document.location);
+    // console.log(window.location);
     if (info.menuItemId === "download-9gag-media") {
-        console.log(info);
+        // console.log(info);
         var url = info.srcUrl;
         var ext = getUrlExtension(url);
-        console.log(fileName,url, ext);
-        if(ext == "mp4" || ext == "jpg"){
+        var mainUrl = "";
+        // console.log(fileName, url, ext);
+        if (ext == "mp4" || ext == "jpg") {
             mainUrl = url;
-        }else if(ext == "webp" || ext == "webm"){
-            if(ext == "webp"){
+        } else if (ext == "webp" || ext == "webm") {
+            if (ext == "webp") {
                 mainUrl = url.substr(0, url.lastIndexOf('.')) + ".jpg";
-            }else{
+            } else {
                 mainUrl = url.substr(0, url.lastIndexOf('.')) + ".mp4";
             }
         }
-        console.log(mainUrl);
-
-        function onStartedDownload(id) {
-            console.log(`Started downloading: ${id}`);
-          }
-          
-          function onFailed(error) {
-            console.log(`Download failed: ${error}`);
-          }
-          
-          var downloadUrl = mainUrl;
-          
-         var fileName = getFileName(mainUrl) + "." + getUrlExtension(mainUrl);
-          var downloading = browser.downloads.download({
-            saveAs: true,
-            filename : fileName,
-            url : downloadUrl,
-            conflictAction : 'uniquify'
-          });
-          
-          downloading.then(onStartedDownload, onFailed);
+        if(mainUrl != ""){
+            var downloadUrl = mainUrl;
+            var fileName = getFileName(mainUrl) + "." + getUrlExtension(mainUrl);
+            var downloading = browser.downloads.download({
+                saveAs: true,
+                filename: fileName,
+                url: downloadUrl
+            });
+            //downloading.then(onStartedDownload, onFailed);
+        }else{
+            console.log("Invalid File");
+        }
     }
 });
 
-function getUrlExtension(url){
-    return url.substr(url.lastIndexOf('.')+1);
+function getUrlExtension(url) {
+    return url.substr(url.lastIndexOf('.') + 1);
 }
-function getFileName(url){
-    return url.substr(url.lastIndexOf('/')+1, url.lastIndexOf('.')-1-url.lastIndexOf('/'));
+
+function getFileName(url) {
+    return url.substr(url.lastIndexOf('/') + 1, url.lastIndexOf('.') - 1 - url.lastIndexOf('/'));
 }
 // https://gist.github.com/Rob--W/ec23b9d6db9e56b7e4563f1544e0d546
 function escapeHTML(str) {
